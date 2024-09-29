1f8e831eeed0c798296d7bb64fec3e4f308dc60d
Elena K <kozlovamgn@gmail.com>
2024-09-29 14:24:22 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Sellers firstSeller = new Sellers();
+        Sellers secondSeller = new Sellers();
+
+        firstSeller.setName("Vaclav");
+        firstSeller.setBirthdate(LocalDate.of(1985,05,5));
+        firstSeller.setAddress("Brno");
+
+        secondSeller.setName("Jan");
+        secondSeller.setBirthdate(LocalDate.of(1987,3,4));
+        secondSeller.setAddress("Pardubice");
+
+        System.out.println();
+        System.out.println("Prodejce mrkve:");
+        System.out.println("name: " + firstSeller.getName() + "; birthdate: " + firstSeller.getBirthdate() + "; address: " + firstSeller.getAddress());
+        System.out.println("name: " + secondSeller.getName() + "; birthdate: " + secondSeller.getBirthdate() + "; address: " + secondSeller.getAddress());
+
     }
 }
--- src/Sellers.java
+++ src/Sellers.java
@@ -1,7 +1,67 @@
 import java.time.LocalDate;
 
-public class Seller1 {
-    private LocalDate.of (1985,12,1);
-    private String sellerEmail;
-    private String sellerPassword;
+public class Sellers {
+    private String name;
+    private LocalDate birthdate;                  //Datum narození prodejce
+    private int numberOfContracts;                //Počet dosud sjednaných smluv
+    private double setTotalCarrotSoldInTons;      //Celkové množství prodané mrkve v tunách
+    private String address;                       //Název města, kde prodejce sídlí
+    private String licensePlate;                  //Registrační značka vozidla („SPZ“)
+    private double FuelConsumption;               //Spotřeba firemního vozidla v litrech na 100km
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getNumberOfContracts() {
+        return numberOfContracts;
+    }
+
+    public void setNumberOfContracts(int numberOfContracts) {
+        this.numberOfContracts = numberOfContracts;
+    }
+
+    public double getSetTotalCarrotSoldInTons() {
+        return setTotalCarrotSoldInTons;
+    }
+
+    public void setSetTotalCarrotSoldInTons(double setTotalCarrotSoldInTons) {
+        this.setTotalCarrotSoldInTons = setTotalCarrotSoldInTons;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public double getFuelConsumption() {
+        return FuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        FuelConsumption = fuelConsumption;
+    }
 }
